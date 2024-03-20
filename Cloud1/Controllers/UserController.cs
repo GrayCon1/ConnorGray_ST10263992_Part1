b769c3f6de5c5959d1602b644aa7085472e7f1bb
lab_services_student <lab_services_student@labVMH8OX>
2024-03-20 16:08:06 +0200

    Add project files.

---
+++ Cloud1/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Cloud1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cloud1.Controllers
+{
+    public class UserController : Controller
+    {
+        public tblUser usrtbl=new tblUser();
+        [HttpPost]
+        public ActionResult About(tblUser Users)
+        {
+            var result = usrtbl.insert_User(Users);
+            return RedirectToAction("Home", "About");
+        }
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View(usrtbl);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cloud1/Models/tblUser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Cloud1.Models
+{
+    public class tblUser
+    {
+        public static string con_string = "Integrated Security = SSPI; Persist Security Info=False;Initial Catalog = test; Data Source = labVMH8OX\\SQLEXPRESS";
+
+        //public static string con_string = "Server=tcp:cldv-sql.database.windows.net,1433;Initial Catalog=cldv-sql-database;Persist Security Info=False;User ID=ConnorG;Password=Dexter3772!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static SqlConnection con =new SqlConnection(con_string);
+
+        public string Name { get;set;}
+        public string Surname { get; set; }
+        public string Email { get;set;}
+        
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public int insert_User(tblUser m)
+        {
+            string sql = "INSERT INTO tblUser (userName, userSurname, userEmail) VALUES (@Name, @Surname, @Email)";
+            SqlCommand cmd = new SqlCommand(sql,con);
+            cmd.Parameters.AddWithValue("@Name", m.Name);
+            cmd.Parameters.AddWithValue("@Surname",m.Surname);
+            cmd.Parameters.AddWithValue("@Email",m.Email);
+            con.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            con.Close();
+            return rowsAffected;
+        }
+    }
+}
